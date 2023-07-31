e74096928fd19bbdac96e2d726f6ffb51074b8ed
Faris aka Animatnetic <faristheknight77@gmail.com>
2023-07-31 10:34:01 +0400

    Removed testing files as they are not needed at least just yet.

---
+++ ToDoStructure.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoStructure.swift
+//  ToDoApp
+//
+//  Created by Faris Ansari on 31/07/2023.
+//
+
+import Foundation
+
+struct ToDo: Identifiable {
+    var id: Int = Int.random(in: 0...100)
+    var
+}
