726e70d74ec75095c273591be4e09762e86f3854
Faris aka Animatnetic <faristheknight77@gmail.com>
2023-07-31 10:15:16 +0400

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Faris Ansari on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
