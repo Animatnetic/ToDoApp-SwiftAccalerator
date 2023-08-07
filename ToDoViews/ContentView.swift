f3b4a451452cb1ff7eca772d76d54ace7f280fee
Faris aka Animatnetic <faristheknight77@gmail.com>
2023-08-07 10:11:19 +0400

    Refactoring challenge complete, refactored ContentView such that each ToDo element is initialised in a new View file

--- ToDoViews/ContentView.swift
+++ ToDoViews/ContentView.swift
@@ -20,51 +20,28 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             VStack {
-                NavigationStack {   
+                NavigationStack {
                     List($tasks, editActions: [.all]) { $task in
-                        NavigationLink {
-                            ToDoDetailedView(todo: $task)
-                        } label: {
-                            HStack {
-                                
-                                Image(systemName: task.isCompleted ? "checkmark.circle.fill" : "circle")
-                                
-                                VStack(alignment: .leading) {
-                                    Text("\(task.title)")
-                                    
-                                    if task.subtitle != "" {
-                                        Text("\(task.subtitle)")
-                                            .font(.caption)
-                                            .foregroundColor(.gray)
-                                    }
-                                }
-                                .strikethrough(task.isCompleted)
+                            ToDoView(toDo: $task)
+                        }
+                        .navigationTitle("To Do:")
+                        .toolbar {
+                            ToolbarItem(placement: .navigationBarLeading) {
+                                EditButton()
                             }
-                            .onTapGesture {
-                                withAnimation {
-                                    task.isCompleted.toggle()
+                            
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                Button {
+                                    showAddSheet = true
+                                } label: {
+                                    Image(systemName: "plus")
+                                        .foregroundColor(.blue)
                                 }
                             }
                         }
                     }
-                    .navigationTitle("To Do:")
-                    .toolbar {
-                        ToolbarItem(placement: .navigationBarLeading) {
-                            EditButton()
-                        }
-                        
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            Button {
-                                showAddSheet = true
-                            } label: {
-                                Image(systemName: "plus")
-                                    .foregroundColor(.blue)
-                            }
-                        }
-                    }
-                }
-            }.sheet(isPresented: $showAddSheet) {
-                NewToDoView(toDosArray: $tasks)
+                }.sheet(isPresented: $showAddSheet) {
+                    NewToDoView(toDosArray: $tasks)
             }
         }
     }
---
+++ ToDoViews/ToDoView.swift
@@ -0,0 +1,45 @@
+//
+//  ToDoView.swift
+//  ToDoApp
+//
+//  Created by Faris Ansari on 07/08/2023.
+//
+
+import SwiftUI
+
+struct ToDoView: View {
+    @Binding var toDo: ToDo
+    
+    var body: some View {
+        NavigationLink {
+            ToDoDetailedView(todo: $toDo)
+        } label: {
+            HStack {
+                
+                Image(systemName: toDo.isCompleted ? "checkmark.circle.fill" : "circle")
+                
+                VStack(alignment: .leading) {
+                    Text("\(toDo.title)")
+                    
+                    if toDo.subtitle != "" {
+                        Text("\(toDo.subtitle)")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                }
+                .strikethrough(toDo.isCompleted)
+            }
+            .onTapGesture {
+                withAnimation {
+                    toDo.isCompleted.toggle()
+                }
+            }
+        }
+    }
+}
+
+struct ToDoView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoView(toDo: .constant(ToDo(title: "Feed the cats")))
+    }
+}
